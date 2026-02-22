9e0be3da3f7e2fa04e9a67d86204a769e1cb1816
Rizqy666 <zaenal.nurrizqy14@gmail.com>
2026-02-22 17:57:10 +0700

    feat(auth): add auth layout template

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+
+<head>
+    @include('partials.head')
+</head>
+
+<body
+    class="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-zinc-900 dark:to-zinc-800 flex items-center justify-center p-4">
+    {{ $slot }}
+
+    @vite(['resources/js/app.js'])
+</body>
+
+</html>
